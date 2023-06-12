873affa1ad50eb8b84e4d7273c40e7b34f324281
Mazhar <65758011+MostafaMazhar@users.noreply.github.com>
2023-06-12 23:26:00 +0000

    added SM variables & ternary oprator

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>"${variables.name} ${variables.lastname}"</h1>
-          <h2>"${variables.role}"r</h2>
-          <h3>"${variables.city}"</h3>
+          <h1>${variables.name || ""} ${variables.lastname || ""}</h1>
+          <h2>${variables.role || ""}</h2>
+          <h3>${variables.city || ""}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            ${
+              variables.twitter
+                ? `<li>
+                <a href="https://twitter.com/${variables.twitter}">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>`
+                : ""
+            }
+            ${
+              variables.github
+                ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.linkedin
+                ? `<li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.instagram
+                ? `<li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>`
+                : ""
+            }
           </ul>
         </div>
     `;
